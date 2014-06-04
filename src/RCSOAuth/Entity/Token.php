a532477abfd40d252751ef73635c7bc4f433d3f3
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-03 19:16:45 -0500

    adding doctrine entity for auth tokens

---
+++ src/RCSOAuth/Entity/Token.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RCSOAuth\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    \Doctrine\Common\Collections\ArrayCollection,
+    \LDNiche\Entity\Niche;
+
+/**
+ * Category table entity.
+ * @ORM\Entity
+ * @ORM\Table(name="rcsoauth_tokens")
+ * @property string $value
+ * @property string $provider
+ * @property date $date
+ * @property int $id
+ */
+class Token extends \RCSBase\Doctrine\Entity
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer");
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $value;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $provider;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    protected $date;
+
+    public function __set($property, $value){
+        $this->$property = $value;
+    }
+
+    public function __get($property){
+        return $this->$property;
+    }
+
+    public function __construct(){
+        parent::__construct();
+        $this->date = new \DateTime();
+    }
+
+}
